624b2b53b9f0b97787aee7a458489c2f65d657f5
Paula Valentina Sánchez Peña <paula.valentinasp@gmail.com>
2020-10-29 09:40:58 -0500

    quiz

---
+++ Quices/Quiz_integrales/quiz_Integrales.R
@@ -0,0 +1,19 @@
+#Puntos
+#(0.1, 1.8), (0.2, 2.6), (0.3, 3.0), (0.4, 2.8), (0.5, 1.9)
+
+library(plotly)
+
+simpson <- function(x, y, n) {
+  
+  s = ((x[n]-x[1])/6) * (y[1] + 4*((x[1]+x[n])/2) +  + y[n])
+  
+  return(s)
+}
+
+x = c(0.1, 0.2, 0.3, 0.4, 0.5)
+y = c(1.8, 2.6, 3.0, 2.8, 1.9)
+n = 5
+cat(simpson(x, y, n))
+cat("\n")
+
+plot(x,y, type = "l")
